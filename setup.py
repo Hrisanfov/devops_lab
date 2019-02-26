f0752eae4e60e082ebff61c7b8bd90ec5e64e70c
student <student@EPBYMINW7423.minsk.epam.com>
2019-02-26 11:16:21 +0300

    “withoutduplicate”

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='task1',
     packages=["task1"],
-    scripts=['task1/bash/python1.sh'],
+    scripts=['python1.sh'],
     author="Ilya_Khrysanfau",
     author_email="Ilya_Khrysanfau@epam.com",
     description="task1 application",
