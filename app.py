5904523128a29576c7073e651fbc5633065c527f
student <student@EPBYMINW7423.minsk.epam.com>
2019-02-26 09:14:56 +0300

    “homework3”

---
+++ app.py
@@ -0,0 +1,44 @@
+import schedule
+import time
+import psutil
+import json
+import datetime
+
+class metrics:
+    "My custom class"
+    cpu = psutil.cpu_percent(interval=1)
+    ram = psutil.disk_usage('/')
+    vram = psutil.virtual_memory()
+    iops = psutil.disk_io_counters(perdisk=False)
+    net = psutil.net_io_counters(pernic=True)
+
+
+i = 0
+txt = "txt"
+f = open("conf.ini")
+data = f.readlines()
+d1 = data[1].split()
+d2 = data[2].split()
+extent = d1[2]
+interval = int(d2[2])
+outfile = "output."
+output = outfile + extent
+test = str(metrics.cpu) + str(metrics.ram) + str(metrics.vram) + str(metrics.iops) + str(metrics.net)
+
+
+def job():
+    time = datetime.datetime.now()
+    if extent == txt:
+        with open(output, "a") as file:
+            print("SNAPSHOT {}: TIMESTAMP{}:\n".format(i + 1, time), test, file=file)
+    else:
+        with open(output, 'a') as fl:
+            print(json.dump(test, fl, ensure_ascii=False))
+
+
+schedule.every(interval).minutes.do(job)
+schedule.every().hour.do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
