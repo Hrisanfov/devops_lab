4a963bc04980be21a5d5416ea873c67da1522503
Hrisanfov <44952076+Hrisanfov@users.noreply.github.com>
2019-02-27 22:21:49 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,13 +1,15 @@
 c = 0
 p = 1
-str = "-7 5 -1 3 9"
+str = "-5 1 2 3 4 5 6 7 8 -3"
 ar = [int(s) for s in str.split(' ')]
-ar = sorted(ar)
-for i in ar:
+ar2 = sorted(ar)
+for i in ar2:
     if i > 0:
         c = c + i
 print(c)
-for k in ar[:-1]:
-    if (k != ar[-1] and k != ar[0]):
-        p = p * k
+armax = ar.index(max(ar))
+armin = ar.index(min(ar))
+for index, item in enumerate(ar):
+    if (armax < index < armin or armin < index < armax):
+        p=p*ar[index]
 print(p)
